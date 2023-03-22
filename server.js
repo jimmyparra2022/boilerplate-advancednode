88e42fac12b4b26af510e60d73b53340d5608a6b
jimmy parra <jimmyparra2022@gmail.com>
2023-03-22 17:52:44 -0400

    avdnode 7.0

--- server.js
+++ server.js
@@ -32,7 +32,15 @@ myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   
   app.route('/').get((req, res) => {
-    res.render('index', { title: 'Connected to Database', message: 'Please log in' });
+    res.render('index', { title: 'Connected to Database', message: 'Please log in', showLogin: true });
+  });
+
+  app.route('login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) {
+    res.redirect('/profile');
+  });
+
+  app.route('profile').get((req, res) => {
+    res.render('profile');
   });
 
   passport.use(new LocalStrategy((username, password,) => {
