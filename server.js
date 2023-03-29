c13b0cde0f65ca8a343a2c0822cda3eb016377c4
jimmy parra <jimmyparra2022@gmail.com>
2023-03-29 09:58:16 -0400

    avdnode 9.0

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ myDB(async client => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('profile');
+    res.render('profile', { username: req.user.username });
   });
 
   passport.use(new LocalStrategy((username, password,) => {
