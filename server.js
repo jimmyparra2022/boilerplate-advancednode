3902417e1879188a5f1b14e530c4092241e1476a
jimmy parra <jimmyparra2022@gmail.com>
2023-08-14 11:48:10 -0400

    advnode 11.0

--- server.js
+++ server.js
@@ -32,8 +32,38 @@ myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   
   app.route('/').get((req, res) => {
-    res.render('index', { title: 'Connected to Database', message: 'Please log in', showLogin: true });
+    res.render('index', { title: 'Connected to Database', message: 'Please log in', showLogin: true, showRegistration: true });
   });
+
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          }
+          )
+        }
+      },
+        passport.authenticate('local', { failureRedirect: '/' },
+        (req, res, next) => {
+          res.redirect('/profile');
+        }
+        )
+      )
+    });
 
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
