44bd03b50f9e49224a396e8fa25bdaa937b904e7
jimmy parra <jimmyparra2022@gmail.com>
2023-08-25 00:21:23 -0400

    advnode 18.3

--- routes.js
+++ routes.js
@@ -74,9 +74,9 @@ module.exports = function (app, myDataBase) {
     app.use((req, res, next) => {
         res.status(404)
             .type('text')
-            .send('Not Found')
+            .send('Not Found');
     });
-    
+
 }
 
 // const ensureAuthenticated = (req, res, next) => {
