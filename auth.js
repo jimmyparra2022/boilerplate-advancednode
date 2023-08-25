f6b090997cbdc7fa3b8fb45fed53f6c2d9d0630b
jimmy parra <jimmyparra2022@gmail.com>
2023-08-24 20:50:11 -0400

    advnode 18.1

--- auth.js
+++ auth.js
@@ -33,13 +33,11 @@ module.exports = function (app, myDataBase) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: 'boilerplate-advancednode.jimmythewebdeve.repl.co/auth/github/callback'
+        callbackURL: 'https://boilerplate-advancednode.jimmythewebdeve.repl.co/auth/github/callback'
     }, function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
-        myDataBase.findOneAndUpdate({
-            id: profile.id
-        }, {
-            $setOnInsert: {
+        myDataBase.findOneAndUpdate({ id: profile.id },
+            { $setOnInsert: {
                 id: profile.id,
                 username: profile.username,
                 name: profile.displayName || 'John Doe',
@@ -48,20 +46,14 @@ module.exports = function (app, myDataBase) {
                     ? profile.emails[0].value
                     : 'No public email',
                 created_on: new Date(),
-                provider: profile.provider || ''
-            },
-            $set: {
-                last_login: new Date()
-            },
-            $inc: {
-                login_count: 1
+                provider: profile.provider || '' },
+            $set: { last_login: new Date() },
+            $inc: { login_count: 1 } },
+            { upsert: true, new: true },
+            (err, doc) => {
+                return cb(null, doc.value);
             }
-        }, {
-            upsert: true,
-            new: true
-        }, (err, doc) => {
-            return cb(null, doc.value)
-        });
+        );
     }));
 
 };
