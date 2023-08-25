f7428bf7c514c81934e39c6c91654db3319f34bc
jimmy parra <jimmyparra2022@gmail.com>
2023-08-25 02:18:06 -0400

    test10

--- server.js
+++ server.js
@@ -34,7 +34,61 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  routes(app, myDataBase);
+  app.route('/').get((req, res) => {
+    res.render('index', {
+        title: 'Connected to Database',
+        message: 'Please log in',
+        showLogin: true,
+        showRegistration: true
+        // showSocialAuth: true
+    });
+});
+
+app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+});
+
+app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile', { username: req.user.username });
+});
+
+app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+app.route('/register')
+.post((req, res, next) => {
+// const hash = bcrypt.hashSync(password, user.password);
+myDataBase.findOne({ username: req.body.username }, (err, user) => {
+    if (err) {
+        next(err);
+    } else if (user) {
+        res.redirect('/');
+    } else {
+        myDataBase.insertOne({
+        username: req.body.username,
+        password: req.body.password
+        },
+        (err, doc) => {
+            if (err) {
+                res.redirect('/');
+            } else {
+            next(null, doc.ops[0]);
+            }
+    }
+    )
+    }
+},
+    passport.authenticate('local', { failureRedirect: '/' },
+    (req, res, next) => {
+    res.redirect('/profile');
+    }
+    )
+)
+});
+
+  // routes(app, myDataBase);
   auth(app, myDataBase);  
 
   let currentUsers = 0;
