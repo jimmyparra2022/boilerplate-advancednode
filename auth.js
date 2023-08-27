968b71cd8fe315831b0c19f1edaf774cbb3e5eab
jimmy parra <jimmyparra2022@gmail.com>
2023-08-27 10:07:57 -0400

    advnode 18.18

--- auth.js
+++ auth.js
@@ -19,16 +19,13 @@ module.exports = function (app, myDataBase) {
 
     passport.use(new LocalStrategy((username, password, done) => {
         myDataBase.findOne({ username: username }, (err, user) => {
-        console.log(`User ${username} attempted to log in.`);
-        if (err) {
-            return done(err);
-        } else if (!user) {
-            return done(null, false);
-        } else if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, false);
-        } else {
-            return done(null, user);
-        }
+            console.log(`User ${username} attempted to log in.`);
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (password !== user.password) {
+            return done(null, false); 
+            } 
+        return done(null, user);
         });
     }));
 
