4dcc94283348a6bef07f6657bb46efc0d101352a
Jimmy Parra <jimmyparra2022@gmail.com>
2023-09-01 06:23:50 -0400

    advnode 18.22

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ myDB(async client => {
   auth(app, myDataBase);  
 
   let currentUsers = 0;
-  io.on('connection', socket => {
+  io.on('connection', (socket) => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
