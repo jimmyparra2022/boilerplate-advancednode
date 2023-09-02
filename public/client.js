db81aaa23ea7d5d7c7782cf1aa2f6b7c02132b12
Jimmy Parra <jimmyparra2022@gmail.com>
2023-09-02 04:20:57 -0400

    advnode 21.0

--- public/client.js
+++ public/client.js
@@ -3,9 +3,14 @@ $(document).ready(function () {
   /*global io*/
   let socket = io();
 
-  socket.on('user count', function (data) {
-    console.log(data);
-  });
+  socket.on('user', function (data) {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message = 
+      data.connected
+        ? ' joined the chat'
+        : ' left the chat'
+    $('#messages').append('<li>').html( '<b>' + data.username + message + ( '</b>' ))
+  })
 
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
--- server.js
+++ server.js
@@ -60,13 +60,21 @@ myDB(async client => {
   let currentUsers = 0;
   io.on('connection', (socket) => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
     console.log('user ' + socket.request.user.username + ' connected');
 
     socket.on('disconnect', () => {
       console.log('user ' + socket.request.user.username + ' disconnected')
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
     });
 
   });
